f7af68f597288512259a872186bd229adb5636fc
amol <amold05@gmail.com>
2026-05-20 20:17:12 +0530

    add

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
-terraform{
-    required_providers{
-        source = "hasicorp/azurerm"
-        version = "4.72.0"
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.73.0"
     }
+  }
 }
 
 provider "azurerm" {
-  features { }
+  features {  }
+  subscription_id = "dd6f64e8-5bcb-4e23-b5d9-781c2b54a2d5"
 }
